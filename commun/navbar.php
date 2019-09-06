c08e8fa254c4c019900873f7ce0318a86a0fc6dc
WEB15 <WEB15@CRPBeauvoir.fr>
2019-09-06 12:08:32 +0200

    affichage d'un message personnel après la connexion

--- commun/navbar.php
+++ commun/navbar.php
@@ -1,41 +1,41 @@
-<!--navbar-->
-
-<?php 
-// var_dump($_SERVER['DOCUMENT_ROOT']);
-// var_dump($_SERVER['HTTP_HOST']);
-// var_dump($_SERVER['SERVER_NAME']);
-// var_dump(__DIR__);
-?>
 <?php include("variables.php"); ?>
 
-<nav class="navbar navbar-light bg-light pb-0">
-    <a class="navbar-brand" href="<?php echo PROJECT_ROOT . 'blog2sneakers.php'?>">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!--mon logo du site-->
+<a class="navbar-brand" href="<?php echo PROJECT_ROOT . 'blog2sneakers.php'?>">
         <img src="<?php echo IMAGES . 'tennis.jpg'?>" width="100" height="60" class="d-inline-block align-top"alt="logo"> <div class="titre">DOSE2SNEAKERS</div>
     </a>
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo PROJECT_ROOT . 'collectionperso.php'?>">Ma collection perso</a>
+    <!--fin logo-->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+          <!--ma collection-->
+      <a class="nav-link" href="<?php echo PROJECT_ROOT . 'collectionperso.php'?>">Ma collection perso</a>
+    </li>
+      <li class="nav-item">
+          <!--les sorties du moment-->
+      <a class="nav-link" href="<?php echo PROJECT_ROOT . 'sortiesdumoment.php'?>">Les sorties du moment</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="<?php echo PROJECT_ROOT . 'sortiesdumoment.php'?>">Les sorties du moment</a>
-        </li>
-        <li class="nav-item">
+            <!--profile-->
             <a class="nav-link" href="<?php echo PROJECT_ROOT . 'contact.php'?>">Profile</a>
         </li>
-        
-            <?php if (isset($_SESSION['pseudo'])): ?>
-    <li class="nav-item">
-        <div class="dropdown">
-        <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <?= $msg = "Bienvenue " . $_SESSION['pseudo'] . "!"; ?>
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-            <button class="dropdown-item px-1 py-0" type="button"><a class="dropdown-item" href="deconnection.php">Me deconnecter</a></button>
-        </div>
-        </div>
-</li>
-        <?php endif; ?>
 
-         
+        <?php if (isset($_SESSION['pseudo'])): ?>
+
+      <li class="nav-item dropdown pb-0 bg-primary rounded">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <?= $msg = "Bienvenue " . $_SESSION['pseudo'] . "!"; ?>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="deconnection.php">Me deconnecter</a>
+      </li>
+
+      <?php endif; ?>
     </ul>
+  </div>
 </nav>
